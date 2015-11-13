aa7b0478673c00fb30eab4bf222e5baa39b5eb68
Dyonisos54 <ge-mini@hotmail.fr>
2015-11-13 13:21:12 +0100

    index primary

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Le Monde </title>
+	<link rel="stylesheet" href="">
+</head>
+	<body>
+
+		<main>
+
+			<section>
+				
+			</section>
+
+			<section>
+
+				<article class="gauche">
+					
+				</article>
+
+				<article class="droite">
+					
+				</article>
+				
+			</section>
+			
+		</main>
+		
+	</body>
+</html>
